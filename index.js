a3423b44f514145d635461c8ce89014c336cf188
sgshujun <js12893@nyu.edu>
2022-10-19 20:44:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+// app.get('/',(request,response)=>{
+//     response.send("Hello");
+// })
+
+// app.get('/about',(request,response)=> {
+//     response.send("this is an about page");
+// })
+
+app.use('/',express.static('public'));
+
+app.listen(3000,()=>{
+    console.log("app is listening at localhost:3000");
+})
